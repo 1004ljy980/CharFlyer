754d308ca0286c1ef04e09d62007ed47334b60eb
JuYoung Lee <lee_ju_0@naver.com>
2023-08-31 18:43:05 +0900

    Design: 전반적인 레이아웃 구조 잡기
    - 커스텀 폰트 사용을 위해 globals.css에 font-face 추가
    - layout.module.scss와 layout.tsx 작성을 통해 헤더의 로고, nav, 푸터 영역 설정
    
    Resolves : #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@
 import '@/app/globals.css';
 import styles from '@/app/layout.module.scss';
 
+// 컴포넌트
+import Link from 'next/link';
+
 // 메타 데이터
 import type { Metadata } from 'next';
 
@@ -19,9 +22,38 @@ export default function RootLayout({
     <html>
       <body>
         <header className={styles.header}>
-          <h1>캐플라이어</h1>
+          <div className={styles.container}>
+            <h1 className={styles.logo}>캐플라이어</h1>
+
+            {/* 일반 메뉴 */}
+            <nav className={styles.leftContainer}>
+              <ul>
+                <li>
+                  <Link href="">메인</Link>
+                </li>
+                <li>
+                  <Link href="">카테고리</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* 사용자 메뉴 */}
+            <nav className={styles.rightContainer}>
+              <ul>
+                <li>
+                  <Link href="">로그인</Link>
+                </li>
+                <li>
+                  <Link href="">회원가입</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </header>
+
         {children}
+
+        <footer className={styles.footer}></footer>
       </body>
     </html>
   );
