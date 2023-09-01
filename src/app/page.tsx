098faa8f721f671af9729eead70679fbfb9b5d30
JuYoung Lee <lee_ju_0@naver.com>
2023-09-01 21:04:09 +0900

    Design: 메인 페이지의 컴포넌트 구조 구성 중
    - CSS코드 구조 통일을 위해 CSS컨벤션에 맞게 속성 순서 조정
    - 메인 페이지(app/page.tsx) 컴포넌트 및 구조 구성 중
    - 메인 캐러셀을 어떻게 작성할지 고민 필요함
    
    Related to : #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
+// 스타일
+import styles from './appPage.module.scss';
+
+const BannerList = () => {
   return <></>;
+};
+
+const SearchBar = () => {
+  return <></>;
+};
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        <section className={styles.bannerSection}>
+          <div>
+            <BannerList />
+          </div>
+          <div>
+            <BannerList />
+          </div>
+        </section>
+        <section className={styles.searchingSection}>
+          <p className={styles.searchTitle}>어떤 캐릭터를 원하세요?</p>
+          <p className={styles.searchSubTitle}>물론, 굿즈도요!</p>
+          <div>
+            <SearchBar />
+          </div>
+          <div>
+            {/* 배경 이미지 */}
+            {/* 종이 배 이미지 */}
+          </div>
+        </section>
+        <section className={styles.recommendedSection}></section>
+        <section className={styles.randomSearchingSection}></section>
+      </main>
+    </>
+  );
 }
